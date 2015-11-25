f12bd07d61395eb9c83e215f5c7dfa98f24cc952
Paulina Kondras <paulina.kondras@gmail.com>
2015-11-25 16:58:52 +0100

    Added random slides presentation

--- slider.js
+++ slider.js
@@ -1,4 +1,12 @@
-function show(nr){
-	
+function showclick(nr){
 	document.getElementById("pic").innerHTML = "<img src=\"Pics/Pic" + nr + ".jpg\" height=\"500\" width=\"500\" alt=\" + nr \" >";
 }
+
+
+function showrandom(){
+	
+	nr = Math.round(Math.random() * 4)+1;
+	document.getElementById("pic").innerHTML = "<img src=\"Pics/Pic" + nr + ".jpg\" height=\"500\" width=\"500\" alt=\" + nr \" >";
+	setTimeout("showrandom()",1000);
+}
+
